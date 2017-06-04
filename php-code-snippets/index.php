f3c5c106aa026b0fe5facd483e5d748efd38ca1f
Bill Hahn <billhahng@gmail.com>
2017-06-04 10:42:14 -0500

    Cleanup index.html and index.php (BH)

--- php-code-snippets/index.php
+++ php-code-snippets/index.php
@@ -42,7 +42,7 @@
        style="cursor:pointer; cursor:hand; border:0"
        />
      </a>
-        <h1>PHP - GCP Autocomplete Ajax UI: GAE + Node.js + SQL Backend!</h1
+        <h1>PHP - GCP Autocomplete Ajax UI: GAE + Node.js + SQL Backend!</h1>
 <!--        <br>
         <h1> Additional Change using Atom on the desktop!!! </h1> -->
     </div>
